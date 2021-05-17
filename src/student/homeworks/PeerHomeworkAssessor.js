6324b0d5dc7083c199e4ef7528cf47863939268f
Dune Thomas <dune.thomas@smartsparrow.com>
2021-05-16 18:05:50 -0700

    Pushing to merge with main.

--- src/student/homeworks/PeerHomeworkAssessor.js
+++ src/student/homeworks/PeerHomeworkAssessor.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import {useDispatch, useSelector} from "react-redux";
 import {ACTIVITY_PROGRESS, HOMEWORK_PROGRESS, MODAL_TYPES, UI_SCREEN_MODES} from "../../app/constants";
 import {Button, Row} from 'react-bootstrap';
-import {API, graphqlOperation} from "aws-amplify";
+import {API} from "aws-amplify";
 import {setActiveUiScreenMode, setCurrentlyReviewedStudentId, updateSingleReview} from "../../app/store/appReducer";
 import {reportError} from "../../developer/DevUtils";
 
@@ -19,9 +19,7 @@ import CommentsPanel from "./CommentsPanel";
 import EditorToolbar, {formats, modules} from "../../tool/RteToolbar";
 import ReactQuill, {Quill} from "react-quill";
 import { v4 as uuid } from "uuid";
-import {updateAssignment as updateAssignmentMutation, updateReview} from "../../graphql/mutations";
-
-const Delta = Quill.import("delta");
+import {updateReview} from "../../graphql/mutations";
 
 library.add(faCheck, faTimes, faGripLines);
 
@@ -49,7 +47,6 @@ function PeerHomeworkAssessor(props) {
   const [prevCommentId, setPrevCommentId] = useState('');
   const [showPlusButton, setShowPlusButton] = useState(false);
   const [activeCommentId, _setActiveCommentId] = useState('');
-
   const [origContent, setOrigContent] = useState(null);
 
   const setActiveCommentId = (id) => {
@@ -58,9 +55,7 @@ function PeerHomeworkAssessor(props) {
   }
 
 
-
   useEffect(() => {
-    console.log(` >>> PeerHomeworkAssessor: []`)
     const tagsElem = document.getElementById('comments-layer-wrapper');
     reactQuillRef.current.editor.addContainer(tagsElem);
 
@@ -71,7 +66,6 @@ function PeerHomeworkAssessor(props) {
     setOrigContent(reactQuillRef.current.editor.getContents(0));
     setUserComments(getInitializedUserComments(review.comments));
 
-    // if (submitBtnRef.current) submitBtnRef.current.onclick = onSubmit;
     onWindowResized();
 
     return () => {
@@ -85,7 +79,6 @@ function PeerHomeworkAssessor(props) {
   }, [props.excessHeight])
 
   useEffect(() => {
-    console.log(` >>> PeerHomeworkAssessor: [activeCommentId]`)
     if (activeCommentId === prevCommentId) return;
 
     if (prevCommentId) {
@@ -101,7 +94,6 @@ function PeerHomeworkAssessor(props) {
 
   useEffect(() => {
     if (!triggerSubmit) return;
-    console.log("SUBMIT PUSHED, YO.", review.criterionRatings)
     saveUpdatesToServer(review, true);
     clearTrigger();
   }, [triggerSubmit])
@@ -128,7 +120,6 @@ function PeerHomeworkAssessor(props) {
     if (origSelection) editor.setSelection(origSelection);
     return(altUserComments)
   }
-
 
   function onWindowResized() {
     // console.log(`props.excessHeight = ${props.excessHeight}`);
@@ -264,11 +255,7 @@ function PeerHomeworkAssessor(props) {
   }
 
 
-
-
   async function saveUpdatesToServer(data, isSubmit = false) {
-
-    // console.log(`saveUpdatesToServer`, data, moment.valueOf());
     if (!data.beganOnDate) data.beganOnDate = moment().valueOf();
     if (isSubmit) data.submittedOnDate = moment().valueOf();
 
@@ -286,94 +273,10 @@ function PeerHomeworkAssessor(props) {
     }));
 
     try {
-      console.log(`using input data: `, inputData);
-      const result = await API.graphql({query: updateReview, variables: {input: inputData}});
-      console.log("result from saveRatingUpdatesToServer()", result)
-
-      console.log(`updateSingleReview() data: `, data);
+      await API.graphql({query: updateReview, variables: {input: inputData}});
       dispatch(updateSingleReview(data))
-
     } catch (error) {
       reportError(error, `We're sorry. An error occurred while trying to save your assessment changes. Please wait a moment and try again.`);
-    }
-  }
-
-
-  // async function saveOrSubmitAssessment(updatedToolHomeworkData, isSubmit) {
-  //   try {
-  //     const updatedHomeworkData = Object.assign({}, homework, {toolHomeworkData: updatedToolHomeworkData});
-  //     const inputData = Object.assign({}, {id: homework.id, toolHomeworkData: updatedToolHomeworkData});
-  //     const result = await API.graphql({query: updateHomeworkReviewData, variables: {input: inputData}});
-  //
-  //     // We must also update the toolAssignmentData to capture the data about if/when an assessment was submitted for this homework
-  //     let isAllocationsUpdated = false, assignmentResult;
-  //     let a = props.allocations.findIndex(a => a.assessorId === activeUser.id && a.homeworkId === homework.id);
-  //     let allocation = props.allocations[a];
-  //     if (!allocation) {
-  //       isAllocationsUpdated = true;
-  //       allocation = {
-  //         assessorId: activeUser.id,
-  //         homeworkId: homework.id,
-  //         beganOnDate: moment().valueOf(),
-  //         submittedOnDate: (isSubmit) ? moment().valueOf() : 0,
-  //       };
-  //     } else if (isSubmit) {
-  //       isAllocationsUpdated = true;
-  //       allocation.submittedOnDate = moment().valueOf();
-  //     }
-  //
-  //     let allocationsNow = [allocation];
-  //     if (isAllocationsUpdated) {
-  //       const allocationResults = await API.graphql(graphqlOperation(getAssignmentAllocations, {id:assignment.id}));
-  //       allocationsNow = [...allocationResults.data.getAssignment.toolAssignmentData.allocations];
-  //       let oldIndex = allocationsNow.findIndex(a => a.assessorId === activeUser.id && a.homeworkId === homework.id);
-  //       if (oldIndex >= 0) {
-  //         allocationsNow.splice(oldIndex, 1, allocation);
-  //       } else {
-  //         allocationsNow.push(allocation);
-  //       }
-  //
-  //       const assignmentInputData = Object.assign({}, {id: assignment.id, toolAssignmentData: {allocations: allocationsNow}});
-  //       console.log('updating allocations', allocationsNow);
-  //       assignmentResult = await API.graphql({query: updateAssignmentAllocations, variables: {input: assignmentInputData}});
-  //     }
-  //
-  //     if (isSubmit) {
-  //       if (result && (!isAllocationsUpdated || (isAllocationsUpdated && assignmentResult))) {
-  //         if (assignment.isUseAutoSubmit) await calcAndSendScore(inputData);
-  //         onReviewUpdated(updatedHomeworkData, isAllocationsUpdated ? allocationsNow : null);
-  //         await setActiveModal({type: MODAL_TYPES.confirmHomeworkSubmitted})
-  //       } else {
-  //         reportError('', `We're sorry. There was a problem submitting your homework for review. Please wait a moment and try again.`);
-  //       }
-  //     } else {
-  //       if (result && (!isAllocationsUpdated || (isAllocationsUpdated && assignmentResult))) {
-  //         onReviewUpdated(updatedHomeworkData, isAllocationsUpdated ? allocationsNow : null);
-  //       } else {
-  //         console.error("problem saving the assessment");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     reportError(error, `We're sorry. There was a problem submitting your homework for review. Please wait a moment and try again.`);
-  //   }
-  // }
-
-  async function calcAndSendScore(homework) {
-    try {
-      const scoreDataObj = {
-        assignmentId: assignment.id,
-        studentId: activeUser.id,
-        scoreGiven: await calcAutoScore(assignment, homework),
-        scoreMaximum: await calcMaxScoreForAssignment(assignment),
-        comment: '',
-        activityProgress: ACTIVITY_PROGRESS.Completed,
-        gradingProgress: HOMEWORK_PROGRESS.fullyGraded
-      };
-
-      console.warn('-----> about to send scoreDataObj: ', scoreDataObj);
-      await sendAutoGradeToLMS(scoreDataObj);
-    } catch(error) {
-      reportError(error, `We're sorry. There was a problem posting your grade`);
     }
   }
 
@@ -383,10 +286,26 @@ function PeerHomeworkAssessor(props) {
     await props.refreshHandler();
   }
 
+  // PRTv2 does not use autoscoring
+  // async function calcAndSendScore(homework) {
+  //   try {
+  //     const scoreDataObj = {
+  //       assignmentId: assignment.id,
+  //       studentId: activeUser.id,
+  //       scoreGiven: await calcAutoScore(assignment, homework),
+  //       scoreMaximum: await calcMaxScoreForAssignment(assignment),
+  //       comment: '',
+  //       activityProgress: ACTIVITY_PROGRESS.Completed,
+  //       gradingProgress: HOMEWORK_PROGRESS.fullyGraded
+  //     };
+  //
+  //     console.warn('-----> about to send scoreDataObj: ', scoreDataObj);
+  //     await sendAutoGradeToLMS(scoreDataObj);
+  //   } catch(error) {
+  //     reportError(error, `We're sorry. There was a problem posting your grade`);
+  //   }
+  // }
 
-  function autoSave() {
-	  // TODO: Bonus. Add in method to handle automatically saving student work
-  }
 
   function renderModal() {
     switch (activeModal.type) {
@@ -439,6 +358,7 @@ function PeerHomeworkAssessor(props) {
       window.removeEventListener('mouseup', onMouseUp);
     }
   }
+
 
 	return (
 		<Fragment>
